d0f6c4abfb5f6df225240280dc79ec862ac0e673
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-11-17 10:55:57 +0200

    Remove unnecessary creating new compatible image on SpriteRenderer

--- src/gameengine/core/components/SpriteRenderer.java
+++ src/gameengine/core/components/SpriteRenderer.java
@@ -26,21 +26,6 @@ public class SpriteRenderer extends DrawComponent {
     public SpriteRenderer(Image image, int layer) {
 
         this.layer = (byte) layer;
-
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice device = env.getDefaultScreenDevice();
-        GraphicsConfiguration config = device.getDefaultConfiguration();
-
-        // Create compatible image
-        BufferedImage newImage = config.createCompatibleImage(
-                image.getWidth(null),
-                image.getHeight(null),
-                Transparency.TRANSLUCENT);
-
-        Graphics g = newImage.getGraphics();
-        g.drawImage(image, 0, 0, null);
-        g.dispose();
-
-        this.image = newImage;
+        this.image = image;
     }
 }
