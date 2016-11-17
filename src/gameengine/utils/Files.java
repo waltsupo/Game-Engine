085541ce922c2d0d050deada465bb271075470cb
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-11-17 10:40:44 +0200

    Add method to load image as it is without scaling

--- src/gameengine/utils/Files.java
+++ src/gameengine/utils/Files.java
@@ -21,12 +21,42 @@ public class Files {
     public static String defaultPath = "src/assets";
 
     /**
-     * Creates compatible image from the source and returns it
+     * Creates compatible image from the source
+     *
+     * @param url Path to image
+     * @return Compatible image
+     * @throws IOException If error getting image
+     */
+    public static Image loadImage(String url) throws IOException {
+
+        Image source = ImageIO.read(new File(url));
+
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = env.getDefaultScreenDevice();
+        GraphicsConfiguration config = device.getDefaultConfiguration();
+
+        // Create compatible image
+        Image image = config.createCompatibleImage(
+                source.getWidth(null),
+                source.getHeight(null),
+                Transparency.TRANSLUCENT);
+
+        Graphics g = image.getGraphics();
+        g.drawImage(source, 0, 0,
+                source.getWidth(null), source.getHeight(null), null);
+        g.dispose();
+
+        return image;
+    }
+
+    /**
+     * Creates compatible image from the source and returns it scaled
      *
      * @param url Path from default path to the image
      * @param width Width of the image to return
      * @param height Height of the image to return
      * @return Image from the source
+     * @throws IOException If error getting image
      */
     public static Image loadImage(String url, int width, int height) throws IOException {
 
