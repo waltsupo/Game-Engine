25d1a099a3977a338a1fbed24570db6367d3e9c4
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-12-17 20:26:50 +0200

    Add Timer-class
    Timer runs given interface's method after given time has passed, can loop.

---
+++ src/gameengine/utils/Timer.java
@@ -0,0 +1,12 @@
+package gameengine.utils;
+
+/**
+ * Does something after given time has passed.
+ *
+ * @author Valtteri Poutanen valtteri.poutanen@hotmail.com
+ * @version 2016.1217
+ * @since 1.7
+ */
+public class Timer {
+
+}
