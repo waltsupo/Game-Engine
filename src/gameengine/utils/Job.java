40ff8e832ef2136f4e3966b7027987718a1347ac
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-12-17 20:30:42 +0200

    Add Job-interface
    Class implementing Job can be run delayed with Timer.

---
+++ src/gameengine/utils/Job.java
@@ -0,0 +1,13 @@
+package gameengine.utils;
+
+/**
+ * Interface for different Jobs done by Timer.
+ *
+ * @author Valtteri Poutanen valtteri.poutanen@hotmail.com
+ * @version 2016.1217
+ * @since 1.7
+ */
+public interface Job {
+
+    void job();
+}
